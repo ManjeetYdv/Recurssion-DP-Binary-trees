8e98df8fa2bc4df61ead1f9a087f7c24caeb0f64
Manjeet Yadav <99329532+ManjeetYdv@users.noreply.github.com>
2022-09-06 23:11:00 +0530

    Update 814. Binary Tree Pruning.java

--- Binary Tree/814. Binary Tree Pruning.java
+++ Binary Tree/814. Binary Tree Pruning.java
@@ -19,16 +19,15 @@ class Solution {
     public TreeNode pruneTree(TreeNode root) {
         
 
-        if(root!=null){
-            
-            root.left = pruneTree(root.left);
-            root.right = pruneTree(root.right);
-            
-            if(root.left==null && root.right==null && root.val==0) return null;
-             
-        }
+       
+        if(root==null) return null;
         
-        return root;
+        root.left = pruneTree(root.left);
+        root.right = pruneTree(root.right);
+            
+        if(root.left==null && root.right==null && root.val==0) return null;
+        else return root;
+        
         
     }
 }
